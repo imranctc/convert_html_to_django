a5187adb9c83c86ab305608c21f0df4363c18409
imranpciu <001ctc@gmail.com>
2023-09-26 11:15:19 +0600

    add admin panel system

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactMessage
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'timestamp')  # Display these fields in the list view
+    list_filter = ('timestamp',)  # Add a filter by timestamp
+    search_fields = ('name', 'email', 'subject', 'message')  # Add a search box for these fields
+    actions = ['delete_selected']  # Enable the delete action
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,4 @@
-# from django.shortcuts import render
 
-# # Create your views here.
-# def index(request):
-#     return render(request, 'index.html')
 from django.shortcuts import render, redirect
 from .models import ContactMessage
 from .forms import ContactForm
@@ -18,3 +14,22 @@ def index(request):
         form = ContactForm()
 
     return render(request, 'index.html', {'form': form})
+
+
+# import json
+# from django.shortcuts import render, redirect
+# from django.http import JsonResponse
+# from .forms import ContactForm
+# def index(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return render(request, 'index.html')
+#         else:
+#             errors = {field: form.errors[field][0] for field in form.errors}
+#             return JsonResponse({'success': False, 'message': 'Form validation errors', 'errors': errors})
+#     else:
+#         form = ContactForm()
+
+#     return render(request, 'index.html', {'form': form})
