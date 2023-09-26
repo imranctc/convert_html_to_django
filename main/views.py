dcc297d2eecbd4f1e4bb9fb03ed4c6e0eec077c6
imranpciu <001ctc@gmail.com>
2023-09-26 10:03:44 +0600

    i make contact form dynamic

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# def index(request):
+#     return render(request, 'index.html')
+from django.shortcuts import render, redirect
+from .models import ContactMessage
+from .forms import ContactForm
+
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'index.html')
+  # Redirect to the same page after successful submission
+    else:
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
